03b726de2fe15029d30a697168bf2c16b901aa43
Arcotsaikiran <arcotsaikiran1611@gmail.com>
2024-07-05 23:55:14 +0530

    added requirements

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from typing import List
+
+
+def get_requirements() -> List[str]:
+
+    requirements_list: List[str] = []
+    
+    return requirements_list
+    
+
+
+
+
+setup(
+    name='sensor',
+    version='0.0.1',
+    author='sam',
+    author_email='arcotsaikiran1611@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements(),
+    
+    
+
+
+)
